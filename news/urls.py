376d4dbc7b86268888243f513e109ef786a931b5
Hetag <khetag.urtayev@mail.ru>
2026-01-02 23:21:00 +0300

    News Portal

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 
 urlpatterns = [
 
-    path('news/', include(('news.news_urls', 'news'), namespace='news')), #namespace='news' возможно лишнее
+    path('news/', include(('news.news_urls', 'news'), namespace='news')),
     path('articles/', include(('news.articles_urls', 'articles'), namespace='articles')),
 ]
